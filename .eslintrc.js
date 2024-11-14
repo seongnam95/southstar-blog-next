bfdbcb4ff0dac7dbe0ab9119a8d3a697c69d1b9c
jsn0509 <jsn_@naver.com>
2024-11-15 02:53:12 +0900

    chore: ESLint, Prettier 구성

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "next",
+    "airbnb",
+    "airbnb/hooks",
+    "airbnb-typescript",
+    "next/core-web-vitals",
+    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  ignorePatterns: [".*.js", "node_modules/", "next.config.js"],
+  rules: {
+    // --- 임시 ---
+    "@typescript-eslint/lines-between-class-members": "off",
+    "@typescript-eslint/no-throw-literal": "off",
+    // -----------
+    "react/jsx-props-no-spreading": "off",
+    "import/prefer-default-export": "off",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        varsIgnorePattern: "^_",
+        argsIgnorePattern: "^_"
+      }
+    ]
+  }
+};
